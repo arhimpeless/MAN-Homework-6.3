67778ce066567d125af655a9a6d41babf6e10ca1
Andrey Markov <arherower@outlook.com>
2022-11-23 17:52:13 +0300

    разбитые методы

--- Program.cs
+++ Program.cs
@@ -4,36 +4,51 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите количество предложений.");
             List<string> offers = new List<string>();
+
+            int numberOffers = GetNumber();
+            GetOffers(offers, numberOffers);
+            RepeatSearch(offers);
+            Output();
+        }
+        private static int GetNumber()
+        {
+            Console.WriteLine("Введите количество предложений.");
             int numberOffers = Convert.ToInt32(Console.ReadLine());
+            return numberOffers;
+        }
+        private static void GetOffers(List<string> offers, int numberOffers)
+        {
             for (int i = 1; i <= numberOffers; i++)
             {
                 Console.Write($"Предложение №{i}: ");
                 offers.Add(Console.ReadLine());
                 Console.WriteLine();
             }
+        }
+        private static void RepeatSearch(List<string> offers)
+        {
+            List<string> result = offers.GroupBy(word => word)
+                        .Where(g => g.Count() > 1)
+                        .Select(word => word.Key)
+                        .ToList();
+            Console.WriteLine(String.Join(", ", result));
+
             //List<string> repeatWords = new List<string>();
             //repeatWords = offers.Where(word => word.Count() > 1).Select(x => x.Key).ToList();
 
+            //var seen = new HashSet<string>();
+            //var duplicates = offers.Where(x => !seen.Add(x));
 
-            //List<string> result = offers.GroupBy(word => word)
-            //            .Where(g => g.Count() > 1)
-            //            .Select(word => word.Key)
-            //            .ToList();
-            //Console.WriteLine(String.Join(", ", result));
+            //Console.WriteLine(String.Join(", ", duplicates));
 
-
-            var seen = new HashSet<string>();
-            var duplicates = offers.Where(x => !seen.Add(x));
-
-            Console.WriteLine(String.Join(", ", duplicates));
-
-
-            //foreach (var repeatWord in repeatWords)
-            //{
-            //    Console.WriteLine(repeatWord);
-            //}
+        }
+        private static void Output()
+        {
+            foreach (var repeatWord in repeatWords)
+            {
+                Console.WriteLine(repeatWord);
+            }
         }
     }
 }
